eebdf1d3a0fdc7354120b0c3960fe384cdd3ffc1
Alexander <alevork@gmail.com>
2023-03-12 01:13:04 +0500

    features: Добавлено чтение и модель для загрузки новостей

--- src/reader.py
+++ src/reader.py
@@ -9,6 +9,7 @@ from collectors.collector import (
     CountryCollector,
     CurrencyRatesCollector,
     WeatherCollector,
+    NewsCollector,
 )
 from collectors.models import (
     CountryDTO,
@@ -16,6 +17,7 @@ from collectors.models import (
     LocationDTO,
     LocationInfoDTO,
     WeatherInfoDTO,
+    NewsDTO,
 )
 
 
@@ -37,12 +39,16 @@ class Reader:
             weather = await self.get_weather(
                 LocationDTO(capital=country.capital, alpha2code=country.alpha2code)
             )
+            news = await self.get_news(
+                LocationDTO(capital=country.capital, alpha2code=country.alpha2code)
+            )
             currency_rates = await self.get_currency_rates(country.currencies)
 
             return LocationInfoDTO(
                 location=country,
                 weather=weather,
                 currency_rates=currency_rates,
+                country_news=news,
             )
 
         return None
@@ -74,6 +80,20 @@ class Reader:
         :return:
         """
         return await WeatherCollector.read(location=location)
+
+    @staticmethod
+    async def get_news(location: LocationDTO) -> list[NewsDTO]:
+        """
+        Получение данных о новостях.
+
+        :param location: Объект локации для получения данных
+        :return:
+        """
+        return [
+            await NewsCollector.read(location=location, number=i)
+            for i in range(3)
+            if i < len(location.news_sources)
+        ]
 
     async def find_country(self, search: str) -> Optional[CountryDTO]:
         """
